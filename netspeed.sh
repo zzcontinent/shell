0f5ea4c4206d0077e6482c5769e06dfb68756fc1
cliff <zzcontinent@163.com>
2019-01-03 10:23:54 +0800

    update

--- netspeed.sh
+++ netspeed.sh
@@ -18,7 +18,23 @@ do
         up_speed=`echo "($up_speed_now-$up_speed_old)/1024"|bc`
 
 
-        #echo "speed : \n\tDN : $[(($down_speed_now-$down_speed_old))] B/s \t $down_speed KB/s"
-        echo -ne "\rDN : $down_speed KB/s UP : $up_speed KB/s"
-        #echo "\tUP : $[(($up_speed_now-$up_speed_old))] B/s \t $up_speed KB/s"
+        echo -ne "\rwifi\tDN : $down_speed KB/s UP : $up_speed KB/s"
+
+
+        #记录初始bytes数目
+        down_speed_old=`ifconfig enx00e04c360031  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
+        up_speed_old=`ifconfig enx00e04c360031  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
+
+        sleep 1
+
+        #保存最新的bytes数目
+        down_speed_now=`ifconfig enx00e04c360031  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
+        up_speed_now=`ifconfig enx00e04c360031  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
+
+        #计算下行与上行的网速
+        down_speed=`echo "($down_speed_now-$down_speed_old)/1024"|bc`
+        up_speed=`echo "($up_speed_now-$up_speed_old)/1024"|bc`
+
+
+        echo -ne "\reth \tDN : $down_speed KB/s UP : $up_speed KB/s"
 done
