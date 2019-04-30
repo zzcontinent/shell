8096f95626300ee886a610791e7abf785c59cd08
cliff <zhaozhizhou@woda.ink>
2019-04-30 11:38:12 +0800

    fix netspeed bug

--- netspeed.sh
+++ netspeed.sh
@@ -1,41 +1,43 @@
-#！/bin/bash
-# version 1　　2017-08-14
 eth_name=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -e '^e.*'`
 wifi_name=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -e '^w.*'`
+echo $eth_name $wifi_name
+echo $@
 while true 
 do
         #记录初始bytes数目
-	down_speed_old=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-        up_speed_old=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
+	if [ "$wifi_name" != "" ];then
+		down_speed_old=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
+        	up_speed_old=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
 
-        sleep 1
+	        sleep 1
 
-        #保存最新的bytes数目
-        down_speed_now=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-        up_speed_now=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
+       		#保存最新的bytes数目
+       		down_speed_now=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
+	        up_speed_now=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
 
-        #计算下行与上行的网速
-        down_speed=`echo "($down_speed_now-$down_speed_old)/1024"|bc`
-        up_speed=`echo "($up_speed_now-$up_speed_old)/1024"|bc`
-
-
-        echo -ne "\rwifi \tDN:$down_speed KB/s\tUP:$up_speed KB/s"
+	        #计算下行与上行的网速
+        	down_speed=`echo "($down_speed_now-$down_speed_old)/1024"|bc`
+	        up_speed=`echo "($up_speed_now-$up_speed_old)/1024"|bc`
 
 
-        #记录初始bytes数目
-        down_speed_old=`ifconfig $eth_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-        up_speed_old=`ifconfig $eth_name | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
+       		echo -ne "\rwifi \tDN:$down_speed KB/s\tUP:$up_speed KB/s"
+	fi
+	if [ "$eth_name" != "" ];then
+	        #记录初始bytes数目
+       		down_speed_old=`ifconfig $eth_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
+	        up_speed_old=`ifconfig $eth_name | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
 
-        sleep 1
+       		sleep 1
 
-        #保存最新的bytes数目
-        down_speed_now=`ifconfig $eth_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-        up_speed_now=`ifconfig $eth_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
+        	#保存最新的bytes数目
+        	down_speed_now=`ifconfig $eth_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
+        	up_speed_now=`ifconfig $eth_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
 
-        #计算下行与上行的网速
-        down_speed=`echo "($down_speed_now-$down_speed_old)/1024"|bc`
-        up_speed=`echo "($up_speed_now-$up_speed_old)/1024"|bc`
+        	#计算下行与上行的网速
+        	down_speed=`echo "($down_speed_now-$down_speed_old)/1024"|bc`
+        	up_speed=`echo "($up_speed_now-$up_speed_old)/1024"|bc`
 
-
-        echo -ne "\reth \tDN:$down_speed KB/s\tUP:$up_speed KB/s"
+        	echo -ne "\reth \tDN:$down_speed KB/s\tUP:$up_speed KB/s"
+	fi
 done
+
