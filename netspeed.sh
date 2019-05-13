d141e5c083ef0dcd53d02476a5a883932346f716
cliff <zhaozhizhou@woda.ink>
2019-05-13 16:47:20 +0800

    add clear printf

--- netspeed.sh
+++ netspeed.sh
@@ -1,10 +1,8 @@
 eth_name=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -e '^e.*'`
 wifi_name=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -e '^w.*'`
-echo $eth_name $wifi_name
-echo $@
 while true 
 do
-        #记录初始bytes数目
+   	#记录初始bytes数目
 	if [ "$wifi_name" != "" ];then
 			down_speed_old_wifi=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
         	up_speed_old_wifi=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
@@ -25,10 +23,6 @@ do
 	        #计算下行与上行的网速
         	down_speed_wifi=`echo "($down_speed_now_wifi-$down_speed_old_wifi)/1024"|bc`
 	        up_speed_wifi=`echo "($up_speed_now_wifi-$up_speed_old_wifi)/1024"|bc`
-
-
-	       	#echo -ne "\rwifi \tDN:$down_speed KB/s\tUP:$up_speed KB/s"
-       		#printf "\rwifi \tDN:%6s KB/s\tUP:%6s KB/s" $down_speed_wifi $up_speed_wifi 
 	fi
 	if [ "$eth_name" != "" ];then
         	#保存最新的bytes数目
@@ -39,9 +33,11 @@ do
         	down_speed_eth=`echo "($down_speed_now_eth-$down_speed_old_eth)/1024"|bc`
         	up_speed_eth=`echo "($up_speed_now_eth-$up_speed_old_eth)/1024"|bc`
 
-        	#echo -ne "\reth \tDN:$down_speed KB/s\tUP:$up_speed KB/s"
-       		#printf "\reth \tDN:%6s KB/s\tUP:%6s KB/s" $down_speed_eth $up_speed_eth 
 	fi
-       		printf "\reth\tDN:%6sKB/s\tUP:%6sKB/s | wifi\tDN:%6sKB/s\tUP:%6sKB/s \r" $down_speed_eth $up_speed_eth $down_speed_wifi $up_speed_wifi 
+			printf "\033c"	
+			echo $eth_name $wifi_name
+			echo $@
+       		printf "eth   DN:%5sKB/s  UP:%5sKB/s \n" $down_speed_eth $up_speed_eth 
+		    printf "wifi  DN:%5sKB/s  UP:%5sKB/s" $down_speed_wifi $up_speed_wifi 
 done
 
