866488440b3f899dce277982d244c343dd8b7d50
zhizhouzhao <zhizhouzhao@deepglint.com>
2021-01-05 18:07:37 +0800

    update netspeed.sh

--- machine_install/res/netspeed.sh
+++ machine_install/res/netspeed.sh
@@ -1,51 +1,56 @@
-eth_name=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -e '^e.*'`
-wifi_name=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -e '^w.*'`
+#!/bin/bash
+net_devs=`ifconfig -s | awk '{i++; if(i>1){print $1}}' | awk '{print $1}'`
+#net_devs=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/[:]*$//g'`
 speed_notify=1024
-while true 
+
+for dev in ${net_devs}
 do
-   	#记录初始bytes数目
-	if [ "$wifi_name" != "" ];then
-			down_speed_old_wifi=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-        	up_speed_old_wifi=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
-	fi
-	if [ "$eth_name" != "" ];then
-	        #记录初始bytes数目
-       		down_speed_old_eth=`ifconfig $eth_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-	        up_speed_old_eth=`ifconfig $eth_name | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
-	fi
-
-	sleep 1
-
-	if [ "$wifi_name" != "" ];then
-       		#保存最新的bytes数目
-       		down_speed_now_wifi=`ifconfig $wifi_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-	        up_speed_now_wifi=`ifconfig $wifi_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
-
-	        #计算下行与上行的网速
-        	down_speed_wifi=`echo "($down_speed_now_wifi-$down_speed_old_wifi)/1024"|bc`
-	        up_speed_wifi=`echo "($up_speed_now_wifi-$up_speed_old_wifi)/1024"|bc`
-	fi
-	if [ "$eth_name" != "" ];then
-        	#保存最新的bytes数目
-        	down_speed_now_eth=`ifconfig $eth_name  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p'`
-        	up_speed_now_eth=`ifconfig $eth_name  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p'`
-
-        	#计算下行与上行的网速
-        	down_speed_eth=`echo "($down_speed_now_eth-$down_speed_old_eth)/1024"|bc`
-        	up_speed_eth=`echo "($up_speed_now_eth-$up_speed_old_eth)/1024"|bc`
-
-	fi
-	printf "\033c"	
-   	printf "$eth_name\tDN:%5sKB/s | UP:%5sKB/s \n" $down_speed_eth $up_speed_eth 
-	printf "$wifi_name\tDN:%5sKB/s | UP:%5sKB/s" $down_speed_wifi $up_speed_wifi 
-	time=`date "+%Y-%m-%d %H:%M:%S"`
-	#if [ $down_speed_wifi -gt $speed_notify -o $up_speed_wifi -gt $speed_notify ];then
-	#	notify-send "$time wifi DN:$down_speed_wifi|UP:$up_speed_wifi" > /dev/null 2>&1
-	#	echo -e "\a"
-	#fi
-	#if [ $down_speed_eth -gt $speed_notify -o $up_speed_eth -gt $speed_notify ];then
-	#	notify-send "$time eth DN:$down_speed_eth|UP:$up_speed_eth" > /dev/null 2>&1
-	#	echo -e "\a"
-	#fi
+	eval rx_pre_${dev}='0'
+	eval tx_pre_${dev}='0'
+	eval rx_cur_${dev}='0'
+	eval tx_cur_${dev}='0'
+	eval rx_speed_${dev}='0'
+	eval tx_speed_${dev}='0'
+	#eval rx_pre_${dev}='123'
+	#echo $(eval echo '$'rx_pre_${dev})
 done
 
+func_step_once()
+{
+	for dev in ${net_devs}
+	do
+		eval rx_cur_${dev}=`ifconfig ${dev}  | sed -n 's/RX.*bytes \([0-9]\+\).*/\1/p' | awk '{print $1}'`
+		eval tx_cur_${dev}=`ifconfig ${dev}  | sed -n 's/TX.*bytes \([0-9]\+\).*/\1/p' | awk '{print $1}'`
+
+		eval rx_speed_${dev}=`echo "($(eval echo '$'rx_cur_${dev})-$(eval echo '$'rx_pre_${dev}))/1024"|bc`
+		eval tx_speed_${dev}=`echo "($(eval echo '$'tx_cur_${dev})-$(eval echo '$'tx_pre_${dev}))/1024"|bc`
+
+		printf "%10s | DN:%10sKB/s | UP:%10sKB/s \n" ${dev} $(eval echo '$'rx_speed_${dev}) $(eval echo '$'tx_speed_${dev})
+		eval rx_pre_${dev}=$(eval echo '$'rx_cur_${dev})
+		eval tx_pre_${dev}=$(eval echo '$'tx_cur_${dev})
+	done
+}
+
+
+func_run()
+{
+	while true
+	do
+		time=`date "+%Y-%m-%d %H:%M:%S"`
+		printf "\033c"
+		func_step_once
+		sleep 1
+		#if [ $down_speed_wifi -gt $speed_notify -o $up_speed_wifi -gt $speed_notify ];then
+		#	notify-send "$time wifi DN:$down_speed_wifi|UP:$up_speed_wifi" > /dev/null 2>&1
+		#	echo -e "\a"
+		#fi
+		#if [ $down_speed_eth -gt $speed_notify -o $up_speed_eth -gt $speed_notify ];then
+		#	notify-send "$time eth DN:$down_speed_eth|UP:$up_speed_eth" > /dev/null 2>&1
+		#	echo -e "\a"
+		#fi
+		sleep 1
+	done
+}
+
+func_step_once
+func_run
