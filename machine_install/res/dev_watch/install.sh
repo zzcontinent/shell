8dcc9d37c0e035e7f8f6485b9d6e11472f8bb06d
zhizhouzhao <zhizhouzhao@deepglint.com>
2021-01-15 18:16:16 +0800

    update dev_watch

--- machine_install/res/dev_watch/install.sh
+++ machine_install/res/dev_watch/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cp -av ./res/* /
+cp ./res/lib/systemd/system/dev_watch.service /lib/systemd/system/
+cp ./res/usr/bin/dev_watch /usr/bin
 systemctl enable dev_watch
 systemctl restart dev_watch
