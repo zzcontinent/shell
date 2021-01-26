3199c7f91fcf1d8269de9375694d427b319dffb7
zhizhouzhao <zhizhouzhao@deepglint.com>
2021-01-26 16:27:30 +0800

    update netspeed.sh

--- machine_install/res/netspeed.sh
+++ machine_install/res/netspeed.sh
@@ -2,7 +2,7 @@
 net_devs=`ifconfig -s | awk '{i++; if(i>1){print $1}}' | awk '{print $1}'`
 #net_devs=`cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/[:]*$//g'`
 speed_notify=1024
-PERIOD=1
+PERIOD=2
 TMP_FILE_FB="${HOME}/.tmp_netspeed"
 [ ! -f ${TMP_FILE_FB} ] && touch ${TMP_FILE_FB}
 
@@ -39,7 +39,7 @@ func_step_total()
 		eval rx_speed_${dev}=`echo "($(eval echo '$'rx_cur_${dev})-$(eval echo '$'rx_pre_${dev}))/1024/${PERIOD}"|bc`
 		eval tx_speed_${dev}=`echo "($(eval echo '$'tx_cur_${dev})-$(eval echo '$'tx_pre_${dev}))/1024/${PERIOD}"|bc`
 
-		printf "%10s | DN:%10sKB/s | UP:%10sKB/s \n" ${dev} $(eval echo '$'rx_speed_${dev}) $(eval echo '$'tx_speed_${dev}) >> ${TMP_FILE_FB}
+		printf "%s | DN:%sKB/s | UP:%sKB/s \n" ${dev} $(eval echo '$'rx_speed_${dev}) $(eval echo '$'tx_speed_${dev}) >> ${TMP_FILE_FB}
 		eval rx_pre_${dev}=$(eval echo '$'rx_cur_${dev})
 		eval tx_pre_${dev}=$(eval echo '$'tx_cur_${dev})
 	done
@@ -61,7 +61,7 @@ func_run()
 		printf "\033c"
 		func_step_total
 		#func_step_pids
-		cat ${TMP_FILE_FB}
+		cat ${TMP_FILE_FB} | column -t
 		#if [ $down_speed_wifi -gt $speed_notify -o $up_speed_wifi -gt $speed_notify ];then
 		#	notify-send "$time wifi DN:$down_speed_wifi|UP:$up_speed_wifi" > /dev/null 2>&1
 		#	echo -e "\a"
