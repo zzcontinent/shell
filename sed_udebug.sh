d0185a759c1a484003fdb72b70f3044722fdaa60
zhizhouzhao <zhizhouzhao@deepglint.com>
2021-11-25 11:06:38 +0800

    add sed_udebug.sh: add udebug before return

--- sed_udebug.sh
+++ sed_udebug.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sed -i 's/^{/{\n\tudebug(\"\\r\\n\");/g' $@
+if [ $1 == '-r' ];then
+	sed -i 's/{udebug.*return\(.*\);}*/return\1;/g' $2
+	sed -i '/udebug(\"\\r\\n\");/d' $2
+else
+	sed -i '/^{/a\\tudebug(\"\\r\\n");' $@
+	sed -i 's/return\(.*\);/{udebug(\"\\r\\n\");return\1;}/g' $@
+fi
