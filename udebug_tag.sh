8fde08a1e648788a3a491656f06c91d70427aea1
zhizhouzhao <zhizhouzhao@deepglint.com>
2021-11-29 11:11:06 +0800

    update udebug_tag.sh

--- udebug_tag.sh
+++ udebug_tag.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 if [ $1 == '-r' ];then
-	sed -i 's/{udebug.*return\(.*\);}*/return\1;/g' $2
-	sed -i '/udebug(\"+++ in *+++\\r\\n\");/d' $2
-	sed -i '/udebug(\"--- out *---\\r\\n\");/d' $2
+	args=($@)
+	unset args[0]
+	sed -i '/^#include \"udebug.h\"/d' ${args[@]}
+	sed -i 's/{udebug.*return \(.*\);}*/return \1;/g' ${args[@]}
+	sed -i '/udebug(\"+++ in *+++\\r\\n\");/d' ${args[@]}
+	sed -i '/udebug(\"--- out *---\\r\\n\");/d' ${args[@]}
 else
+	sed -i '1i#include \"udebug.h\"' $@
 	sed -i '/^{/a\\tudebug(\"+++ in  +++\\r\\n");' $@
-	sed -i 's/return\(.*\);/{udebug(\"--- out ---\\r\\n\");return\1;}/g' $@
+	sed -i 's/return \(.*\);/{udebug(\"--- out ---\\r\\n\");return \1;}/g' $@
 	sed -i  '/^}$/i\\tudebug(\"--- out ---\\r\\n\");' $@
 fi
