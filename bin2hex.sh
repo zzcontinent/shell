9c81ae6182f2ed75bde893b193c6439a23a8dea8
zzcontinent <zzcontinent@163.com>
2022-05-30 15:56:20 +0800

    update bin2hex hex2bin

--- bin2hex.sh
+++ bin2hex.sh
@@ -3,4 +3,5 @@ INPUNT=$1
 printf "============%64s\n" '|-60|-56|-52|-48|-44|-40|-36|-32|-28|-24|-20|-16|-12|--8|--4|--0'
 printf "============%64s\n" $(echo "obase=2;ibase=16;8888888888888888"|bc)
 printf "input (bin):%64s\n" $(echo "obase=2;ibase=2;${INPUNT}"|bc)
+printf "input (dec):%64s\n" $(echo "obase=10;ibase=2;${INPUNT}"|bc)
 printf "output(hex):%64s\n" $(echo "obase=16;ibase=2;${INPUNT}"|bc)
--- hex2bin.sh
+++ hex2bin.sh
@@ -3,5 +3,6 @@ INPUNT=$(printf '%x' $1 | tr a-z A-Z)
 printf "============%64s\n" '|-60|-56|-52|-48|-44|-40|-36|-32|-28|-24|-20|-16|-12|--8|--4|--0'
 printf "============%64s\n" $(echo "obase=2;ibase=16;8888888888888888"|bc)
 printf "input (hex):%64s\n" $(echo "obase=16;ibase=16;${INPUNT}"|bc)
+printf "input (dec):%64s\n" $(echo "obase=10;ibase=16;${INPUNT}"|bc)
 printf "output(bin):%64s\n" $(echo "obase=2;ibase=16;${INPUNT}"|bc)
 
