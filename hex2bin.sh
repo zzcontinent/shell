a4934c819af591f8a864295395899c77f3ad0f30
zzcontinent <zzcontinent@163.com>
2022-08-27 17:59:39 +0800

    up hex2bin bin2hex

--- hex2bin.sh
+++ hex2bin.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 INPUNT=$(printf '%x' $1 | tr a-z A-Z)
-echo '-----------------------------------------------------------------------------'
+printf "============%64s\n" $(echo "obase=2;ibase=16;8888888888888888"|bc)
+printf "============%64s\n" '|-60|-56|-52|-48|-44|-40|-36|-32|-28|-24|-20|-16|-12|--8|--4|--0'
 printf "input (hex):%64s\n" $(echo "obase=16;ibase=16;${INPUNT}"|bc)
-printf "input (dec):%64s\n" $(echo "obase=10;ibase=16;${INPUNT}"|bc)
-echo '-----------------------------------------------------------------------------'
-printf "            %64s\n" '|-60|-56|-52|-48|-44|-40|-36|-32|-28|-24|-20|-16|-12|--8|--4|--0'
 printf "output(bin):%64s\n" $(echo "obase=2;ibase=16;${INPUNT}"|bc)
-echo '-----------------------------------------------------------------------------'
+printf "output(oct):%64s\n" $(echo "obase=8;ibase=16;${INPUNT}"|bc)
+printf "output(dec):%64s\n" $(echo "obase=10;ibase=16;${INPUNT}"|bc)
 
