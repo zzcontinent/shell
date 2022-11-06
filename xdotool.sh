8178fa911523fd1bf27e5a405629d38aa2da604d
zzcontinent <zzcontinent@163.com>
2022-11-06 14:04:17 +0800

    [add] xdotool support keyboard control mouse move

---
+++ xdotool.sh
@@ -0,0 +1,6 @@
+#xdotool mousemove_relative -- -10 0  //left move 10
+#xdotool mousemove_relative -- 10  0  //right move 10
+
+#xdotool click 1 //click
+#xdotool click --repeat 2 1 //double click
+#xdotool click 3 //right click
