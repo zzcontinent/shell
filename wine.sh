2b69009bd955a2fc17df5b690989aeb5a0dbad6f
zzcontinent <zzcontinent@163.com>
2024-02-04 00:10:14 +0800

    [up] wine fonts

--- wine.sh
+++ wine.sh
@@ -3,3 +3,6 @@
 pacman -S wine
 
 cd ${WINEPREFIX:-~/.wine}/drive_c/windows/Fonts && for i in /usr/share/fonts/**/*.{ttf,otf}; do ln -s "$i"; done
+winetricks corefonts
+winetricks allfonts
+winetricks gdiplus riched20 riched30
