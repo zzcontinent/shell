dc1a2d3e5c1ca6b476a7e0899681229bb36ade08
zzcontinent <zzcontinent@163.com>
2024-02-18 17:17:06 +0800

    [up] clash_bg.sh

--- clash_bg.sh
+++ clash_bg.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-[ "x$1" == "xkill" ] && pkill -e -f clash-linux-amd64 && exit 0
-
-echo -n > ~/.clash.log
-clash-linux-amd64 -f /home/backup/pi/wiki/clash.yaml.txt >> ~/.clash.log &
+if [ "x$1" == "xkill" ];then
+	pkill -e -f clash-linux-amd64
+	exit 0
+else
+	echo -n > ~/.clash.log
+	clash-linux-amd64 -f /home/backup/pi/wiki/clash.yaml.txt >> ~/.clash.log &
+fi
 
