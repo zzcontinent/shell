f887efb4b164b88c2e20bb6c21c136b7aade6564
zzcontinent <zzcontinent@163.com>
2024-05-23 17:24:28 +0800

    [add] freq.sh

---
+++ freq.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cpucnt=$1
+freqmin=$2
+freqmax=$3
+
+if [ -z $(which cpupower 2>/dev/null) ];then
+	if [ $# == 0 ];then
+		cpufreq-info
+		exit 0
+	elif [ $# != 3 ];then
+		echo "freq.sh cpucnt minfreq maxfreq"
+		exit 1
+	else
+		for i in `seq 0 $(expr ${cpucnt} - 1)`
+		do
+			cpufreq-set -c $i --min ${freqmin} --max ${freqmax}
+		done
+	fi
+else
+	if [ $# == 0 ];then
+		cpupower frequency-info
+		exit 0
+	elif [ $# != 3 ];then
+		echo "freq.sh cpucnt minfreq maxfreq"
+		exit 1
+	else
+		for i in `seq 0 $(expr ${cpucnt} - 1)`
+		do
+			cpupower -c $i frequency-set --min ${freqmin} --max ${freqmax}
+		done
+	fi
+fi
+
+
