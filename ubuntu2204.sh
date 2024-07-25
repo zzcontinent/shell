69af37967863cbcb5a3b4b2c45812e9104c9374d
zzcontinent <zzcontinent@163.com>
2024-07-25 15:22:43 +0800

    [add] ubuntu

---
+++ ubuntu2204.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo $#
+
+if [ $# -eq 0 ];then
+	echo "./run.sh r  -- run docker ubuntu from images"
+	echo "./run.sh e  -- exec into container bash"
+	exit 1
+elif [ x"$1" == "xr" ];then
+	docker run -itd \
+		-v /etc/localtime:/etc/localtime:ro \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-e DISPLAY=unix$DISPLAY \
+		-e GDK_SCALE \
+		-e GDK_DPI_SCALE \
+		--name ubuntu2204 \
+		--mount type=bind,source=/home/cliff/workspace/docker_workspace,target=/mnt,consistency=consistent \
+		ubuntu:22.04
+		#--rm \
+elif [ x"$1" == "xe" ];then
+	docker exec -it ubuntu2204 bash
+fi
+
